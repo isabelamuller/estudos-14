0d8cf58dd428ade559687aacfa4cd7c908456ee3
Isabela Müller <isabelamuller1@hotmail.com>
2021-12-20 16:59:13 -0300

    a

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Integer int1 = 212;
+        Double dou1 = 4.99;
+        Character char1 = 'K';
+        String str1 = int1.toString();
+        System.out.println(str1.length()); // transformei o int1 em uma string e printa o tamanho dela (3 pois 3 numeros 212)
+        System.out.println(int1); // printa o numero e so it goes
+        System.out.println(dou1);
+        System.out.println(char1);
+    }
+}
